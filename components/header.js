df973789c42d7f6ef4f4af2d70299dd9dfa0f316
“Christopher <christopherhileweb@gmail.com>
2023-04-22 14:43:35 -0400

    adjustments

---
+++ components/header.js
@@ -0,0 +1,13 @@
+
+export default function Header() {
+    return (
+        <>
+            <h1>Barnes General Contractracting</h1>
+            <h3>Tipp City, Ohio</h3>
+            <div>
+                <a href="tel:9375243332">(937)524-3332</a>
+                <a href="mailto:bruce@bgctippcity.com">bruce@bgctippcity.com</a>
+            </div>
+        </>
+    );
+}
