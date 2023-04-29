db715800ff20092c240a0e771d93e7a52551ad25
“Christopher <christopherhileweb@gmail.com>
2023-04-29 10:32:55 -0400

    homepage

--- components/header/header.js
+++ components/header/header.js
@@ -11,7 +11,7 @@ const Header = () => {
                 <Image src={logo} alt="Logo for BGC" className={styles.image}/>
                 <div>
                     <h1 className={`${styles.h1} ${anton.className}`}>Barnes General Contracting</h1>
-                    <h3 className={styles.h3}>Tipp City, Ohio</h3>
+                    <h2 className={styles.h2}>Tipp City, Ohio</h2>
                     <div className={styles.contact}>
                         <a href="tel:9375243332">(937)524-3332</a>
                         <a href="mailto:bruce@bgctippcity.com">bruce@bgctippcity.com</a>
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,48 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.scss'
+import left from '../public/images/hammer.jpg'
+import right from '../public/images/hammer_plier.png'
+import hvac from '../public/images/hvac.jpg'
+import waterHeater from '../public/images/water_heater.jpg'
+import electrical from '../public/images/electric.jpg'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Barnes General Contracting</title>
-        <meta name="description" content="Bruce Barnes General Contracting Tipp City, Ohio" />
+        <meta name="description" content="Bruce Barnes General Contracting Tipp City, Ohio. Handyman. Fully insured." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
      <main>
-      
+        <div className={styles.container}>
+          <Image src={left} alt="hammer nails on board" className={styles.image} />
+          <div>
+            <h2 className={styles.h2}>Fully Insured</h2>
+            <h2 className={styles.h2}>Handyman</h2>
+            <p className={styles.p}>Available for small jobs.</p>
+            <p className={styles.p}>A day or two turnaround.</p>
+          </div>
+          <Image src={right} alt="hammer, pliers, paint stirrer" className={styles.image} />
+        </div>
+        <h2 className={styles.h2}>Specialized in:</h2>
+        <div className={styles.container}>
+          <p className={styles.p}>Heating and Air Conditioning</p>
+          <Image src={hvac} alt="heating/ac display unit" className={styles.image} />
+        </div>
+        <div className={styles.container}>
+          <Image src={waterHeater} alt="man repairing water heater" className={styles.image} />
+          <p className={styles.p}>Water Heater</p>
+        </div>
+        <div className={styles.container}>
+          <p className={styles.p}>Electrical Work</p>
+          <Image src={electrical} alt="electrical equipment" className={styles.image} />
+        </div>
+        <div className={styles.container}>
+          <p className={styles.p}>And Much More</p>
+        </div>
      </main>
     </>
   )
