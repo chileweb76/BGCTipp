87e3d167c1fafcf68b9e3320ea3168eeebf1fb37
“Christopher <christopherhileweb@gmail.com>
2023-04-30 15:44:16 -0400

    without nodemailer

--- pages/contact.js
+++ pages/contact.js
@@ -1,24 +1,44 @@
+import { useRef } from 'react';
 import { zillaSlab } from '@/utils/fonts';
 import styles from '../styles/Contact.module.scss'
 import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
-    const [submitted, setSubmitted] = useState(false);
     const [error, setError] =useState('');
     const [response, setResponse] = useState('');
  
+    const form = useRef(); 
+
     const handleSubmit = async (e) => {
         e.preventDefault()
         console.log('Sending')
+       
+        
+        emailjs.sendForm('service_eek62sq','template_dwt3cp2', form.current, '3fYt6b_rKsQ3P-_eS')
+      .then((result) => {
+          console.log(result.text);
+          console.log('Response succeeded!')
+              setName('')
+              setEmail('')
+              setMessage('')
+          setResponse("Thank you! Talk to you soon!")
+      }, (error) => {
+          console.log(error.text);
+          setError("Sorry there was an error please attempt again or call 937-524-3332")
+      });
+
+
+   /* Code for NodeMailer with contact API     
         let data = {
             name,
             email,
             message
         }
-        fetch('/api/contact', {
+       fetch('/api/contact', {
             method: 'POST',
             body: JSON.stringify(data)
           }).then((res) => {
@@ -33,7 +53,8 @@ const Contact = () => {
             } else {
                 setError("Sorry there was an error please attempt again or call 937-524-3332")
             }
-          })
+         })
+         */
     }
 
     return (
@@ -41,7 +62,7 @@ const Contact = () => {
             <h2 className={styles.h2}>Contact</h2>
                 <h3 className={styles.h3}>{response}</h3>
                 <p className={styles.p}>{error}</p>
-                < form className={styles.main} onSubmit={(e)=>{handleSubmit(e)}}  >
+                < form ref={form} className={styles.main} onSubmit={(e)=>{handleSubmit(e)}}  >
                 < div className={styles.inputGroup} >
                     < label htmlFor='name' className={styles.inputLabel}>Name</label>
                     < input type='text' onChange={(e)=>{setName(e.target.value)}} name='name' value={name} className={styles.inputField} />  
